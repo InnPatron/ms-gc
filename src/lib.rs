1b6c5cfcdaa4f78c6757fb079b32aa8b20971a04
Alexander Vo <randomcivvy0121@gmail.com>
2018-08-07 12:52:58 -0700

    Use the allocator API

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::ptr;
 use std::mem;
+use std::alloc::{alloc, dealloc, Layout};
 
 pub struct GC {
     allocd: *mut ObjHeader,
@@ -14,30 +15,33 @@ impl GC {
         }
     }
 
-    fn alloc_obj<T>(&self, data: T) -> (*mut Obj<T>, *mut ObjHeader) {
-        let obj_ptr = Box::into_raw(Box::new(Obj {
+    unsafe fn alloc_obj<T>(&self, data: *const T) -> (*mut Obj<T>, *mut ObjHeader) {
+        let fake_ptr = data as *const Obj<T>;
 
-            header: ObjHeader {
-                next: ptr::null_mut(),
-                block_size: mem::size_of::<T>(),
-            },
+        // Layout for Obj<T>
+        let layout = Layout::for_value(&*fake_ptr);
 
-            data: data,
-        }));
-
-        let header_ptr: *mut ObjHeader = unsafe {
-                &mut ((*obj_ptr).header) as *mut ObjHeader
+        // Allocate Obj<T>
+        let obj_ptr = { 
+            let ptr = alloc(layout) as *mut Obj<T>;
+            (*ptr).header.reachable = true;
+            (*ptr).header.next = ptr::null_mut();
+            (*ptr).header.block_size = mem::size_of::<T>();
+            ptr
         };
+
+        let header_ptr: *mut ObjHeader = &mut ((*obj_ptr).header) as *mut ObjHeader;
 
         (obj_ptr, header_ptr)
     }
 
     pub fn alloc<T>(&mut self, data: T) -> GCObj<T> {
-
+        let (obj_ptr, header_ptr) = unsafe { self.alloc_obj(&data as *const T) };
+        unsafe { 
+            (*obj_ptr).data = data;
+        }
         if self.allocd.is_null() {
             // No allocd objects
-
-            let (obj_ptr, header_ptr) = self.alloc_obj(data);
 
             self.allocd = header_ptr;
             self.tail = header_ptr;
@@ -48,27 +52,27 @@ impl GC {
         } else {
             // Some allocd objects
             
-            unsafe {
-                // Alloc new object and insert at the end of the list
-                let (obj_ptr, header_ptr) = self.alloc_obj(data);
-                
+            // Alloc new object and insert at the end of the list
+            unsafe {    
                 (*self.tail).next = header_ptr;
-                self.tail = header_ptr;
+            }
+            self.tail = header_ptr;
 
-                GCObj {
-                    obj: obj_ptr,
-                }
+            GCObj {
+                obj: obj_ptr,
             }
         }
     }
 }
 
+#[derive(Copy, Clone)]
 pub struct GCObj<T> {
     obj: *mut Obj<T>,
 }
 
 #[repr(C)]
 struct ObjHeader {
+    reachable: bool,
     next: *mut ObjHeader,
     block_size: usize,
 }
